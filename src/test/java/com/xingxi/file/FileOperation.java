79d24b9396aae2c38ec5259f010550b05b244656
ccwhale <392629711@qq.com>
2019-03-07 01:57:29 +0800

    fileOperation

--- src/test/java/com/xingxi/file/FileOperation.java
+++ src/test/java/com/xingxi/file/FileOperation.java
@@ -2,8 +2,12 @@ package com.xingxi.file;
 
 import junit.framework.TestCase;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
 /**
  * @author cc
@@ -39,10 +43,37 @@ public class FileOperation extends TestCase {
     public void testFileUtils() throws IOException {
         FileUtils.readFileToString(new File("/Users/cc/Downloads/1.txt"),"UTF-8");
         FileUtils.readLines(new File("/Users/cc/Downloads/1.txt"),"UTF-8");
+
+        File file = new File("/commons/io/project.properties");
+        List lines = FileUtils.readLines(file, "UTF-8");
     }
 
     public void testMoveFile() throws IOException {
         FileUtils.moveDirectory(new File("/Users/cc/Downloads/3.txt"),
                 new File("/Users/cc/Downloads/4.txt"));
+    }
+
+    public void testNet() throws IOException {
+        InputStream in = new URL( "http://commons.apache.org" ).openStream();
+        try {
+            InputStreamReader inR = new InputStreamReader( in );
+            BufferedReader buf = new BufferedReader( inR );
+            String line;
+            while ( ( line = buf.readLine() ) != null ) {
+                System.out.println( line );
+            }
+        } finally {
+            in.close();
+        }
+    }
+
+    public void testIoUtils() throws IOException {
+        InputStream in = new URL( "http://commons.apache.org" ).openStream();
+        try {
+            System.out.println( IOUtils.toString( in ) );
+        } finally {
+            IOUtils.closeQuietly(in);
+        }
+
     }
 }
